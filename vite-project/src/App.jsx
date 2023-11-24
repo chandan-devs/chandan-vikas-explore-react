c75db489c1342a6225e8756e0cb281d3f7eb9a61
chandan-devs <chandangupta.devs@gmail.com>
2023-11-25 02:05:52 +0530

    Add git config --global user.name

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,9 @@
+import Section from "./components/Section"
+
 function App() {
   return (
     <>
+      <Section />
       <h1>Build react using vite</h1>
     </>
   )
---
+++ vite-project/src/components/Section.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Section() {
+  return (
+    <div>
+      <h1>This is section </h1>
+    </div>
+  )
+}
+
+export default Section
